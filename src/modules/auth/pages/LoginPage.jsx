dc1a780ee4ed49fa500d3149371f01e19bd75ce3
Tanvir Rahman Khan <aeontanvir@gmail.com>
2023-06-14 11:25:33 -0500

    Login and RegisterPage added

--- src/modules/auth/pages/LoginPage.jsx
+++ src/modules/auth/pages/LoginPage.jsx
@@ -1,9 +1,70 @@
-import React from 'react'
+import React, { useState } from "react";
 
-function LoginPage() {
+const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add your login logic here
+    console.log("Email:", email);
+    console.log("Password:", password);
+    // Reset form fields
+    setEmail("");
+    setPassword("");
+  };
+
   return (
-    <div>LoginPage</div>
-  )
-}
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-purple-500 to-indigo-600">
+      <div className="bg-white w-80 p-6 rounded-lg shadow-lg">
+        <h2 className="text-3xl font-bold text-center text-gray-800 mb-6">
+          Login
+        </h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-gray-700 font-medium mb-2"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="border border-gray-300 rounded-lg px-4 py-2 w-full focus:outline-none focus:border-indigo-500"
+              placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              htmlFor="password"
+              className="block text-gray-700 font-medium mb-2"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="border border-gray-300 rounded-lg px-4 py-2 w-full focus:outline-none focus:border-indigo-500"
+              placeholder="Enter your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="bg-indigo-500 hover:bg-indigo-600 text-white font-medium py-2 px-4 rounded-lg w-full transition-colors duration-300"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default LoginPage
+export default LoginPage;
--- src/modules/auth/pages/RegisterPage.jsx
+++ src/modules/auth/pages/RegisterPage.jsx
@@ -1,9 +1,90 @@
-import React from 'react'
+import React, { useState } from "react";
 
-function RegisterPage() {
+const RegisterPage = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add your registration logic here
+    console.log("Name:", name);
+    console.log("Email:", email);
+    console.log("Password:", password);
+    // Reset form fields
+    setName("");
+    setEmail("");
+    setPassword("");
+  };
+
   return (
-    <div>RegisterPage</div>
-  )
-}
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-purple-500 to-indigo-600">
+      <div className="bg-white w-80 p-6 rounded-lg shadow-lg">
+        <h2 className="text-3xl font-bold text-center text-gray-800 mb-6">
+          Register
+        </h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label
+              htmlFor="name"
+              className="block text-gray-700 font-medium mb-2"
+            >
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              className="border border-gray-300 rounded-lg px-4 py-2 w-full focus:outline-none focus:border-indigo-500"
+              placeholder="Enter your name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-gray-700 font-medium mb-2"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="border border-gray-300 rounded-lg px-4 py-2 w-full focus:outline-none focus:border-indigo-500"
+              placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              htmlFor="password"
+              className="block text-gray-700 font-medium mb-2"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="border border-gray-300 rounded-lg px-4 py-2 w-full focus:outline-none focus:border-indigo-500"
+              placeholder="Enter your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="bg-indigo-500 hover:bg-indigo-600 text-white font-medium py-2 px-4 rounded-lg w-full transition-colors duration-300"
+          >
+            Register
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default RegisterPage
+export default RegisterPage;
